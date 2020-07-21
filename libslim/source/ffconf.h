34b2e0d9cf1aa70bd524f6c18636fa7aba510bad
Ronny Chan <ronny6993@gmail.com>
2020-07-21 02:07:04 -0400

    Try enabling FS lock

--- libslim/source/ffconf.h
+++ libslim/source/ffconf.h
@@ -266,7 +266,7 @@
 */
 
 
-#define FF_FS_LOCK		0
+#define FF_FS_LOCK		120
 /* The option FF_FS_LOCK switches file lock function to control duplicated file open
 /  and illegal operation to open objects. This option must be 0 when FF_FS_READONLY
 /  is 1.
