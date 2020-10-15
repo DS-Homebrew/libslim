02af631ae05802db287c0e056a48c4ddf819a653
Ronny Chan <ronny6993@gmail.com>
2020-10-15 16:55:21 -0400

    Fix licenses

--- libslim/source/elm.c
+++ libslim/source/elm.c
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2009-2011, yellow wood goblin
+Copyright (c) 2020, chyyran
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -9,7 +10,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the yellow wood goblin nor the
+    * Neither the name of the copyright holder nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
